e6b1b9d021778f9889e19d73aa9725a2dcdf4aa1
Thales Nunes <thalesaknunes22@gmail.com>
2021-07-10 00:37:54 -0300

    rofi fix in audio out

--- rofi/.config/rofi/modi/AudioOut.sh
+++ rofi/.config/rofi/modi/AudioOut.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-source="$(pactl list short sinks | cut -f 2 | rofi -dpi 1 -dmenu -p "Change audio:")";
-inputs="$(pactl list sink-inputs short | cut -f 1)";
+if [ ! -z "$@" ]
+then
+    # Handle argument.
+    if [ -n "$@" ]
+    then
+        source="$@";
+        inputs="$(pactl list sink-inputs short | cut -f 1)";
 
-for input in $inputs; do
-  pactl move-sink-input "$input" "$source";
-done
+        for input in $inputs; do
+            pactl move-sink-input "$input" "$source";
+        done
 
-pactl set-default-sink "$source";
+        pactl set-default-sink "$source";
+    fi
+else
+    echo "$(pactl list short sinks | cut -f 2)"
+fi
+
