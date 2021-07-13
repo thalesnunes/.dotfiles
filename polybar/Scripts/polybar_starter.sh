2f156c3c6acc9ef9fbacfaaf2d0d457d2efefa2b
Thales Nunes <thalesaknunes22@gmail.com>
2021-07-13 00:05:30 -0300

    finished ricing polybar and added day and night themes

--- polybar/Scripts/polybar_starter.sh
+++ polybar/Scripts/polybar_starter.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+DIR="${HOME}/.dotfiles/polybar/.config/polybar/"
+result=$(~/Scripts/sunrise-sunset.sh)
+IFS=" " read -r sunrise sunset <<< "$result"
+
+now=$(date +%H:%M)
+
+if [[ "$now" > "$sunrise" ]] && [[ "$now" < "$sunset" ]]; then
+    COLORS=colors.day
+else
+    COLORS=colors.night
+fi
+
+cp $DIR/forest/$COLORS $DIR/forest/colors.ini
 ${HOME}/.config/polybar/launch.sh --forest > /dev/null 2>&1 &
