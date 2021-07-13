bd35e19c9fb23e4152565501519b4445fc0972c0
Thales Nunes <thalesaknunes22@gmail.com>
2021-07-13 00:06:09 -0300

    change of wallpaper on sunrise/sunset

---
+++ feh/Scripts/feh_init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="/home/thales/Imagens/Wallpapers"
+day="79_nologo.jpg"
+night="HD KDE Plasma Abstract 345 NO LOGO.png"
+
+result=$(~/Scripts/sunrise-sunset.sh)
+IFS=" " read -r sunrise sunset <<< "$result"
+
+now=$(date +%H:%M)
+
+if [[ "$now" > "$sunrise" ]] && [[ "$now" < "$sunset" ]]; then
+    Wallpaper=$day
+else
+    Wallpaper=$night
+fi
+
+feh --no-fehbg --bg-scale "$DIR/$Wallpaper"
+
