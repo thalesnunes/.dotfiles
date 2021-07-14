948f51a78a5a554b2e3651b60b45cf8953433b97
Thales Nunes <thalesaknunes22@gmail.com>
2021-07-14 13:12:25 -0300

    setting zsh variables for editor and visual

--- zsh/.oh-my-zsh/custom/exports.zsh
+++ zsh/.oh-my-zsh/custom/exports.zsh
@@ -2,3 +2,11 @@ export DOCKER_FORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{
 export PROJECTS='/home/thales/Projects'
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$PATH:$PYTHON_BIN_PATH"
+
+if [ -x "$(command -v nvim)" ]; then
+    export EDITOR='nvim'
+    export VISUAL='nvim'
+else
+    export EDITOR='vim'
+    export VISUAL='vim'
+fi
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -95,7 +95,6 @@ source $ZSH/oh-my-zsh.sh
 # else
 #   export EDITOR='mvim'
 # fi
-export EDITOR='nvim'
 
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"
