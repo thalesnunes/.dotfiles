364faf8b49605a11ae2cb968bac6dc553976c888
Thales Nunes <thalesaknunes22@gmail.com>
2021-07-31 22:31:02 -0300

    new wallpapers

--- feh/Scripts/feh_init.sh
+++ feh/Scripts/feh_init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 DIR="/home/thales/Imagens/Wallpapers"
-day="79_nologo.jpg"
-night="HD KDE Plasma Abstract 345 NO LOGO.png"
+day="cropped-1920-1080-679478.jpg"
+night="cropped-1920-1080-959309.png"
 
 result=$(cat ~/.sun)
 IFS=" " read -r sunrise sunset <<< "$result"
@@ -15,4 +15,4 @@ else
     Wallpaper=$night
 fi
 
-feh --no-fehbg --bg-fill "$DIR/$Wallpaper" "$DIR/pawel-nolbert-4u2U8EO9OzY-unsplash.jpg"
+feh --no-fehbg --bg-fill "$DIR/$Wallpaper"
