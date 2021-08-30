ca579ca92313e846ce65a4d3e99e2b900f41dead
Thales Nunes <thalesaknunes22@gmail.com>
2021-08-29 23:30:11 -0300

    find hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -21,5 +21,6 @@ require('telescope').setup {
 
 -- Find files using Telescope command-line sugar.
 V.keymap('n', '<leader>ff', ':Telescope find_files<CR>')
-V.keymap('n', '<leader>fg', ':Telescope live_grep<CR>')
+V.keymap('n', '<leader>fa', ':Telescope find_files hidden=true <CR>')
+V.keymap('n', '<leader>fw', ':Telescope live_grep<CR>')
 V.keymap('n', '<leader>b', ':Telescope buffers<CR>')
