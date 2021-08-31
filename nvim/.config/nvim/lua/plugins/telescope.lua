7b3f07aaa49cc8d52de0a02306bfb4695a080f8b
Thales Nunes <thalesaknunes22@gmail.com>
2021-08-30 23:03:43 -0300

    changed binds that are not used that often

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -20,7 +20,7 @@ require('telescope').setup {
 }
 
 -- Find files using Telescope command-line sugar.
-V.keymap('n', '<leader>ff', ':Telescope find_files<CR>')
-V.keymap('n', '<leader>fa', ':Telescope find_files hidden=true <CR>')
-V.keymap('n', '<leader>fw', ':Telescope live_grep<CR>')
+V.keymap('n', '<leader>sf', ':Telescope find_files<CR>')
+V.keymap('n', '<leader>sa', ':Telescope find_files hidden=true <CR>')
+V.keymap('n', '<leader>sw', ':Telescope live_grep<CR>')
 V.keymap('n', '<leader>b', ':Telescope buffers<CR>')
