0c3e99e9361e2f59dcb447c6da661ca026a689b4
Thales Nunes <thalesaknunes22@gmail.com>
2021-08-31 19:23:59 -0300

    neoclip on visual mode

--- nvim/.config/nvim/lua/plugins/neoclip.lua
+++ nvim/.config/nvim/lua/plugins/neoclip.lua
@@ -3,3 +3,4 @@ require('neoclip').setup()
 
 -- Clipboard manager
 V.keymap('n', '<C-p>', ':lua require("telescope").extensions.neoclip.default()<CR>')
+V.keymap('v', '<C-p>', ':lua require("telescope").extensions.neoclip.default()<CR>')
