347865c1801864352e8b418885899606a6fc960b
Thales Nunes <thalesaknunes22@gmail.com>
2021-08-30 21:02:23 -0300

    binds for all numbers in list

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -2,8 +2,8 @@
 V.keymap('n', '<leader>a', ':lua require("harpoon.mark").add_file()<CR>')
 V.keymap('n', '<C-e>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
 
-V.keymap('n', '<leader>1', ':lua require("harpoon.ui").nav_file(1)<CR>')
-V.keymap('n', '<leader>2', ':lua require("harpoon.ui").nav_file(2)<CR>')
-V.keymap('n', '<leader>3', ':lua require("harpoon.ui").nav_file(3)<CR>')
-V.keymap('n', '<leader>4', ':lua require("harpoon.ui").nav_file(4)<CR>')
+for num = 1,9,1 do
+    key = tostring(num)
+    V.keymap('n', '<leader>'..key, ':lua require("harpoon.ui").nav_file('..key..')<CR>')
+end
 V.keymap('n', '<leader>t', ':lua require("harpoon.term").gotoTerminal(1)<CR>')
