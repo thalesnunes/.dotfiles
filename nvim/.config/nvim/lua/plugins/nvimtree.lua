c63902eb9476088c49ca5b9f1269d9370111f54f
Thales Nunes <thalesaknunes22@gmail.com>
2021-09-01 22:07:46 -0300

    Personal configs for nvim-tree

--- nvim/.config/nvim/lua/plugins/nvimtree.lua
+++ nvim/.config/nvim/lua/plugins/nvimtree.lua
@@ -1,30 +1,26 @@
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 
-V.g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-V.g.nvim_tree_allow_resize = 1
-V.g.nvim_tree_auto_close = 0 -- closes tree when it's the last window
-V.g.nvim_tree_auto_ignore_ft = { "dashboard" } -- don't open tree on specific fiypes.
-V.g.nvim_tree_auto_open = 0
-V.g.nvim_tree_disable_netrw = 0
-V.g.nvim_tree_follow = 1
-V.g.nvim_tree_git_hl = 1
-V.g.nvim_tree_gitignore = 0
-V.g.nvim_tree_hide_dotfiles = 0
-V.g.nvim_tree_highlight_opened_files = 0
-V.g.nvim_tree_hijack_netrw = 0
-V.g.nvim_tree_indent_markers = 1
-V.g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
-V.g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
-V.g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
-V.g.nvim_tree_side = "left"
-V.g.nvim_tree_tab_open = 0
-V.g.nvim_tree_update_cwd = 1
+-- set initial width
 V.g.nvim_tree_width = 25
-V.g.nvim_tree_lsp_diagnostics = 0
+-- ignore dirs
+V.g.nvim_tree_ignore = { ".git", "node_modules", ".cache", "__pycache__" }
+-- opens the tree when typing `vim $DIR` or `vim`
+V.g.nvim_tree_auto_open = 1
+-- don't auto open tree on specific filetypes.
+V.g.nvim_tree_auto_ignore_ft = { "startify", "dashboard" }
+-- closes the tree when you open a file
+V.g.nvim_tree_quit_on_open = 1
+-- this option shows indent markers when folders are open
+V.g.nvim_tree_indent_markers = 1
+-- will enable file highlight for git attributes (can be used without the icons).
+V.g.nvim_tree_git_hl = 1
+-- will enable folder and file icon highlight for opened files/directories.
+V.g.nvim_tree_highlight_opened_files = 1
+-- root folder format
+V.g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
 
 V.g.nvim_tree_show_icons = {
    folders = 1,
-   -- folder_arrows= 1
    files = 1,
    git = 1,
 }
@@ -42,11 +38,8 @@ V.g.nvim_tree_icons = {
       untracked = "★",
    },
    folder = {
-      -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
-      -- arrow_open = "",
-      -- arrow_closed = "",
       default = "",
-      empty = "", -- 
+      empty = "",
       empty_open = "",
       open = "",
       symlink = "",
