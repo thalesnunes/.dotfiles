6b99661c18ee4bd18fe6c75ac93bbd48844b5557
Thales Nunes <thalesaknunes22@gmail.com>
2021-09-03 20:18:02 -0300

    FINALLY got light and dark mode to work properly with a python-crontab script.

---
+++ random/Scripts/ctab.py
@@ -0,0 +1,33 @@
+#!/bin/python
+import os
+from pathlib import Path
+
+from crontab import CronTab
+
+ROOT = Path(os.path.expanduser('~'))
+USER = os.environ.get('USER')
+
+with open(ROOT/".sun") as sunfile:
+    sunrise, sunset = sunfile.read().split()
+
+light_hour, light_min = sunrise.split(":")
+dark_hour, dark_min = sunset.split(":")
+
+with CronTab(user=USER) as cron:
+
+    light, dark = None, None
+    for job in cron.find_command("light-dark-mode"):
+        if job.comment == "light mode":
+            light = job
+        elif job.comment == "dark mode":
+            dark = job
+
+    if not (light and dark):
+        raise Exception("Couldn't find light and dark cronjobs")
+
+    light.hour.on(light_hour)
+    light.minute.on(light_min)
+    dark.hour.on(dark_hour)
+    dark.minute.on(dark_min)
+
+    cron.write()
