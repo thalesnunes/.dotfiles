25f1a6cf068c2721590346997e589929f2cdd274
Thales Nunes <thalesaknunes22@gmail.com>
2021-09-10 18:11:00 -0300

    Removed enter for choosing autocomplete

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -16,10 +16,6 @@ cmp.setup {
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true,
-        })
     },
     -- You should specify your *installed* sources.
     sources = {
