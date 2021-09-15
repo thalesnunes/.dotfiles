54a2ce9183b3620814f8a7ab3dbc058a0abe1795
Thales Nunes <thalesaknunes22@gmail.com>
2021-09-14 21:18:58 -0300

    Better keybinds for harpoon

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -2,8 +2,12 @@
 V.keymap('n', '<leader>a', ':lua require("harpoon.mark").add_file()<CR>')
 V.keymap('n', '<C-e>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>')
 
-for num = 1,9,1 do
-    key = tostring(num)
-    V.keymap('n', '<leader>'..key, ':lua require("harpoon.ui").nav_file('..key..')<CR>')
-end
+-- for num = 1,9,1 do
+--     key = tostring(num)
+--     V.keymap('n', '<leader>'..key, ':lua require("harpoon.ui").nav_file('..key..')<CR>')
+-- end
+V.keymap('n', '<c-j>', ':lua require("harpoon.ui").nav_file(1)<CR>')
+V.keymap('n', '<c-k>', ':lua require("harpoon.ui").nav_file(2)<CR>')
+V.keymap('n', '<c-l>', ':lua require("harpoon.ui").nav_file(3)<CR>')
+V.keymap('n', '<c-m>', ':lua require("harpoon.ui").nav_file(4)<CR>')
 V.keymap('n', '<leader>t', ':lua require("harpoon.term").gotoTerminal(1)<CR>')
