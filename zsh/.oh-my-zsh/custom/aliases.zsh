c172a32860e1b4f9400f1c88a81c2ec7ca868547
Thales Nunes <thalesaknunes22@gmail.com>
2021-09-15 00:05:27 -0300

    Moved main functions and aliases to the bin folder, so I can run it from anywhere without needing zsh

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -7,9 +7,6 @@ alias lla='ls -la'
 alias aptup='sudo apt update && sudo apt upgrade'
 alias yayup='yay -Syu'
 
-alias dockerinit='sudo systemctl start docker.socket && sudo systemctl start docker'
-alias dockerstop='sudo systemctl stop docker && sudo systemctl stop docker.socket'
-
 alias gs='git status'
 alias gd='git diff'
 alias ga='git add'
@@ -23,9 +20,6 @@ alias por='poetry run'
 
 alias stown='stow --no-folding -nvt ~'
 alias stowv='stow --no-folding -vt ~'
-
-alias spotifydinit='systemctl --user start spotifyd.service'
-alias spotifydstop='systemctl --user stop spotifyd.service'
 
 alias gcalp='gcalcli --config-folder ~/.config/gcalcli/personal'
 alias gcalw='gcalcli --config-folder ~/.config/gcalcli/work'
--- zsh/.oh-my-zsh/custom/functions.zsh
+++ zsh/.oh-my-zsh/custom/functions.zsh
@@ -2,73 +2,10 @@ function gi() {
     curl -sL https://www.toptal.com/developers/gitignore/api/$@ >> .gitignore ;
 }
 
-function updatedb() {
-    cd ~/Projects/db-crawler
-    echo "Starting daily update"
-    for file in `ls -1 scripts | sed -e 's/\..*$//'`;
-    do
-        echo "\nRunning $file";
-        time pipenv run python -c "from scripts import $file";
-    done
-    echo "\nFinished updating"
-    cd "$OLDPWD"
-}
-
-function pginit() {
-    OS=$(lsb_release -is)
-    case $OS in
-        Ubuntu)
-            sudo systemctl start apache2 &&
-            echo "Started successfully" &&
-            google-chrome-stable 'localhost/pgadmin4' &!
-            ;;
-        ManjaroLinux)
-            dockerinit &&
-            docker start pgadmin &&
-            echo "Started successfully" &&
-            sleep 1 &&
-            google-chrome-stable 'localhost' &!
-            ;;
-        *)
-            echo "OS not supported"
-            ;;
-    esac
-}
-
-function pgstop() {
-    OS=$(lsb_release -is)
-    case $OS in
-        Ubuntu)
-            sudo systemctl stop apache2 &&
-            echo "Stopped successfully"
-            ;;
-        ManjaroLinux)
-            docker stop pgadmin &&
-            dockerstop &&
-            echo "Stopped successfully"
-            ;;
-        *)
-            echo "OS not supported"
-            ;;
-    esac
-}
-
 function create() {
     cd ~/Projects/Project_Initializer/
     pipenv run python3 ~/Projects/Project_Initializer/create.py $@
     cd "$OLDPWD"
-}
-
-function dbcrawler() {
-    cd ~/Projects/db-crawler
-    terminal -e jupyter console --kernel=dbcrawler
-    pipenv run $EDITOR
-    cd "$OLDPWD"
-}
-
-function dbconsole() {
-    cd ~/Projects/db-crawler
-    jupyter console --kernel=dbcrawler
 }
 
 function dbcrawler_docker() {
@@ -78,68 +15,4 @@ function dbcrawler_docker() {
     sleep 2 &&
     jupyter_url=$(docker logs dbcrawler | grep -o "http:\/\/127\.0\.0\.1:8888\/lab?token=.*" | tail -1) &&
     google-chrome $jupyter_url &!
-}
-
-function config() {
-    DOT="$HOME/.dotfiles"
-    TOCD=$DOT
-    FILE=""
-    PROG=$1
-    case $PROG in
-        poly*)
-            TOCD="$DOT/polybar/.config/polybar/forest"
-            FILE=""
-            ;;
-        ali*)
-            TOCD="$DOT/zsh/.oh-my-zsh/custom"
-            FILE="aliases.zsh"
-            ;;
-        func*)
-            TOCD="$DOT/zsh/.oh-my-zsh/custom"
-            FILE="functions.zsh"
-            ;;
-        exp*)
-            TOCD="$DOT/zsh/.oh-my-zsh/custom"
-            FILE="exports.zsh"
-            ;;
-        tok*)
-            TOCD="$DOT/zsh/.oh-my-zsh/custom"
-            FILE="tokens.zsh"
-            ;;
-        *)
-            if [ -d $DOT/$PROG ]; then
-                TOCD="$DOT/$PROG"
-                if [ -d $TOCD/.config ]; then
-                    TOCD="$TOCD/.config"
-                    if [ -d $TOCD/$PROG ]; then
-                        TOCD="$TOCD/$PROG"
-                    fi
-                fi
-            fi
-            files=$(ls -A1q $TOCD)
-            if [ $(echo $files | wc -l) -eq 1 ]; then
-                FILE=$files
-            fi
-            ;;
-    esac
-
-    cd $TOCD
-    $EDITOR $FILE
-    cd "$OLDPWD"
-}
-
-function proj() {
-    PROJ=$PROJECTS
-    TOCD=`ls -1q $PROJ | fzf`
-    if [[ -n "$TOCD" ]]; then
-        cd $PROJ/$TOCD
-        if [[ -f "poetry.lock" ]]; then
-            poetry run $EDITOR
-        elif [[ -f "Pipfile.lock" ]]; then
-            pipenv run $EDITOR
-        else
-            $EDITOR
-        fi
-        cd "$OLDPWD"
-    fi
 }
