489c15133d81bb6e6e9b33d1faf8bb5a88c2f668
Thales Nunes <thalesaknunes22@gmail.com>
2021-09-16 19:43:33 -0300

    Moved cli-used tools to functions, instead of bash scripts

--- zsh/.oh-my-zsh/custom/functions.zsh
+++ zsh/.oh-my-zsh/custom/functions.zsh
@@ -16,3 +16,71 @@ function dbcrawler_docker() {
     jupyter_url=$(docker logs dbcrawler | grep -o "http:\/\/127\.0\.0\.1:8888\/lab?token=.*" | tail -1) &&
     google-chrome $jupyter_url &!
 }
+
+function config() {
+    DOT="$HOME/.dotfiles"
+    TOCD=$DOT
+    FILE=""
+    PROG=$1
+    case $PROG in
+        bin)
+            TOCD="$DOT/bin/.local/bin"
+            FILE=""
+            ;;
+        poly*)
+            TOCD="$DOT/polybar/.config/polybar/forest"
+            FILE=""
+            ;;
+        ali*)
+            TOCD="$DOT/zsh/.oh-my-zsh/custom"
+            FILE="aliases.zsh"
+            ;;
+        func*)
+            TOCD="$DOT/zsh/.oh-my-zsh/custom"
+            FILE="functions.zsh"
+            ;;
+        exp*)
+            TOCD="$DOT/zsh/.oh-my-zsh/custom"
+            FILE="exports.zsh"
+            ;;
+        tok*)
+            TOCD="$DOT/zsh/.oh-my-zsh/custom"
+            FILE="tokens.zsh"
+            ;;
+        *)
+            if [ -d $DOT/$PROG ]; then
+                TOCD="$DOT/$PROG"
+                if [ -d $TOCD/.config ]; then
+                    TOCD="$TOCD/.config"
+                    if [ -d $TOCD/$PROG ]; then
+                        TOCD="$TOCD/$PROG"
+                    fi
+                fi
+            fi
+            files=$(ls -A1q $TOCD)
+            if [ $(echo "$files" | wc -l) -eq 1 ]; then
+                FILE=$files
+            fi
+            ;;
+    esac
+
+    cd $TOCD
+    $EDITOR $FILE
+    cd "$OLDPWD"
+}
+
+function proj(){
+    PROJ=$PROJECTS
+    TOCD=`ls -1q $PROJ | fzf`
+    if [[ -n "$TOCD" ]]; then
+        cd $PROJ/$TOCD
+        if [[ -f "poetry.lock" ]]; then
+            poetry run $EDITOR
+        elif [[ -f "Pipfile.lock" ]]; then
+            pipenv run $EDITOR
+        else
+            $EDITOR
+        fi
+    fi
+    cd "$OLDPWD"
+}
