b16d21fdec6aef63a9ba82cbd9e68c0870e63ebf
Thales Nunes <thalesaknunes22@gmail.com>
2021-09-19 01:21:14 -0300

    Disabled builtin plugins, not used

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -30,6 +30,31 @@ V.opt.colorcolumn = '80'
 V.opt.laststatus = 2
 V.opt.showmode = false
 
+-- Disable distribution plugins
+local disabled_built_ins = {
+  'netrw',
+  'netrwPlugin',
+  'netrwSettings',
+  'netrwFileHandlers',
+  'gzip',
+  'zip',
+  'zipPlugin',
+  'tar',
+  'tarPlugin',
+  'getscript',
+  'getscriptPlugin',
+  'vimball',
+  'vimballPlugin',
+  '2html_plugin',
+  'logipat',
+  'rrhelper',
+  'spellfile_plugin',
+  'matchit'
+}
+for _, plugin in pairs(disabled_built_ins) do
+  V.g['loaded_' .. plugin] = 1
+end
+
 V.cmd [[
     syntax enable
     colorscheme dracula
