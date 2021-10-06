5d7e6ffb8e4199bf4cc895cbd6d73f60319657fa
Thales Nunes <thalesaknunes22@gmail.com>
2021-10-06 00:46:30 -0300

    Added func for buffer keymap and removed comments

--- nvim/.config/nvim/lua/helpers.lua
+++ nvim/.config/nvim/lua/helpers.lua
@@ -7,27 +7,16 @@ M.g = vim.g
 M.execute = vim.api.nvim_command
 M.lsp = vim.lsp
 
--- function M.is_buffer_empty() return vim.fn.empty(vim.fn.expand('%:t')) == 1 end
-
--- function M.has_width_gt(cols) return vim.fn.winwidth(0) / 2 > cols end
-
 function M.keymap(mode, new_keymap, command, opts)
     local options = {noremap = true, silent = true}
     if opts then options = vim.tbl_extend('force', options, opts) end
     vim.api.nvim_set_keymap(mode, new_keymap, command, options)
 end
 
--- function M.dump(o)
---     if type(o) == 'table' then
---         local s = '{ '
---         for k, v in pairs(o) do
---             if type(k) ~= 'number' then k = '"' .. k .. '"' end
---             s = s .. '[' .. k .. '] = ' .. M.dump(v) .. ','
---         end
---         return s .. '} '
---     else
---         return tostring(o)
---     end
--- end
+function M.buf_keymap(buf, mode, new_keymap, command, opts)
+    local options = {noremap = true, silent = true}
+    if opts then options = vim.tbl_extend('force', options, opts) end
+    vim.api.nvim_buf_set_keymap(buf, mode, new_keymap, command, options)
+end
 
 return M
