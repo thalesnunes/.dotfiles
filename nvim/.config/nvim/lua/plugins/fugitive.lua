34709cc8c0d1674812c1aedbc2f5b2e41f2dfbbd
Thales Nunes <thalesaknunes22@gmail.com>
2021-10-10 00:08:17 -0300

    removed unused plugins configs

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++
@@ -1,7 +0,0 @@
-V.keymap('n', '<leader>ga', ':Git<CR>')
-V.keymap('n', '<leader>gc', ':Git commit<CR>')
-V.keymap('n', '<leader>gp', ':Git push<CR>')
-V.keymap('n', '<leader>gl', ':Git log<CR>')
-
--- TODO: add git checkout with user prompt
--- V.keymap('n', '<leader>gh', ':Git push<CR>')
