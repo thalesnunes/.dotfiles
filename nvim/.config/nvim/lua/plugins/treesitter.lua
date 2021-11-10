d95fc0b47795f070cdfd7e3bf22e278b035e480e
Thales Nunes <thalesaknunes22@gmail.com>
2021-11-09 23:58:26 -0300

    feat: rainbow config in treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -8,4 +8,10 @@ require('nvim-treesitter.configs').setup {
     highlight = {
         enable = true,              -- false will disable the whole extension
     },
+    rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = 5000,
+        -- colors = {},
+    }
 }
