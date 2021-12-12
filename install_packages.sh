d850ec7613b61e999be4038c22bd2094f8367efc
Thales Nunes <thalesaknunes22@gmail.com>
2021-12-11 23:14:02 -0300

    feat: change shell to zsh

--- install_packages.sh
+++ install_packages.sh
@@ -51,6 +51,7 @@ if is_installed "zsh"; then
         git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+        chsh -s "$(which zsh)"
     fi
 fi
 
