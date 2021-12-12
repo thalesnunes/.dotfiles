3ca777812cc876f109250e5ae726eb00a6e9a631
Thales Nunes <thalesaknunes22@gmail.com>
2021-12-11 22:59:38 -0300

    feat: install powerlevel10k

--- install_packages.sh
+++ install_packages.sh
@@ -50,6 +50,7 @@ if is_installed "zsh"; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
         git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
     fi
 fi
 
