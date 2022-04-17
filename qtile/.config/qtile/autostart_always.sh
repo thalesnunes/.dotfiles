f90f7ab90e127ed45a9a348d1b17fe9d8105b62b
Thales Nunes <thalesaknunes22@gmail.com>
2022-04-17 17:24:56 -0300

    fix: delete unused TWM

--- qtile/.config/qtile/autostart_always.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-xrandr --output VGA-0 --off --output DVI-D-0 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-0 --primary --mode 1920x1080 --pos 0x215 --rotate normal
-# $HOME/Scripts/polybar_starter.sh &
-$HOME/Scripts/feh_init.sh &
