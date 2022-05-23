40b5644ba0baa47e6dda931410a6b26b621a8ae8
Thales Nunes <thalesaknunes22@gmail.com>
2022-05-22 22:53:17 -0300

    feat: delete playlist entries with Ctrl+d over the name

--- rofi/.config/rofi/modi/MPV.sh
+++ rofi/.config/rofi/modi/MPV.sh
@@ -10,16 +10,25 @@ fi
 if [ ! -z "$@" ]; then
     # Handle argument.
     if [ -n "$@" ]; then
-        while read line; do
-            if [[ $line == "$@"* ]]; then
-                playlist_dir="$(echo $line | sed 's/.*=//')"
-                if [[ $playlist_dir == "/run/media/thales/External HD"* ]]; then
-                    udisksctl mount --object-path block_devices/sdb2 &> /dev/null
-                fi
-                coproc mpv -- "$playlist_dir" &> /dev/null
-            fi
-        done < "$history_file"
+        case "$ROFI_RETV" in
+            1)  # Selected entry with Enter
+                while read line; do
+                    if [[ $line == "$@"* ]]; then
+                        playlist_dir="$(echo $line | sed 's/.*=//')"
+                        if [[ $playlist_dir == "/run/media/thales/External HD"* ]]; then
+                            udisksctl mount --object-path block_devices/sdb2 &> /dev/null
+                        fi
+                        coproc mpv -- "$playlist_dir" &> /dev/null
+                    fi
+                done < "$history_file"
+                ;;
+            10)
+                sed -i "/$1=.*/d" "$history_file"
+                sed 's/=.*//' "$history_file" | sort
+                ;;
+        esac
     fi
 else
+    echo -en "\0use-hot-keys\x1ftrue\n"
     sed 's/=.*//' "$history_file" | sort
 fi
