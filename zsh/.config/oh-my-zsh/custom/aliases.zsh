42582147745c8f77d30182c5c8655027905d1fa3
Thales Nunes <thalesaknunes22@gmail.com>
2022-07-23 19:17:34 -0300

    fix: unused alias

--- zsh/.config/oh-my-zsh/custom/aliases.zsh
+++ zsh/.config/oh-my-zsh/custom/aliases.zsh
@@ -35,5 +35,4 @@ alias dockerps='docker ps -a --format=$DOCKER_FORMAT'
 alias yt_down='yt-dlp -o "~/Downloads/%(title)s.%(ext)s" -f "[ext=mp4][height<=1080]"'
 
 # HOME cleanup
-alias svn="svn --config-dir $XDG_CONFIG_HOME/subversion"
 alias wget="wget --hsts-file=$XDG_DATA_HOME/wget-hsts"
