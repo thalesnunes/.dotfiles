9071dfeb070faeac93899a765afe1390d14be445
Thales Nunes <thalesaknunes22@gmail.com>
2024-03-04 11:37:47 -0300

    feat: add bind to open multiple files in nvim pickers

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -34,6 +34,20 @@ require('telescope').setup {
                 ['<C-j>'] = 'move_selection_next',
                 ['<C-k>'] = 'move_selection_previous',
                 ['<ESC>'] = 'close',
+                ['<CR>'] = function(prompt_bufnr)
+                        local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+                        local multi = picker:get_multi_selection()
+                        if not vim.tbl_isempty(multi) then
+                            require('telescope.actions').close(prompt_bufnr)
+                            for _, j in pairs(multi) do
+                                if j.path ~= nil then
+                                    vim.cmd(string.format('%s %s', 'edit', j.path))
+                                end
+                            end
+                        else
+                            require('telescope.actions').select_default(prompt_bufnr)
+                        end
+                    end
             },
             n = {
                 ['g'] = 'move_to_top',
