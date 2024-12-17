534a335b5922fdd7499850860c927e803191bb63
Thales Nunes <thalesaknunes22@gmail.com>
2024-12-17 16:44:32 -0300

    fix: run snowsql in ubuntu strat

--- zsh/.config/zsh/imports/aliases.zsh
+++ zsh/.config/zsh/imports/aliases.zsh
@@ -38,7 +38,7 @@ alias yt_down='yt-dlp -o "~/Downloads/%(title)s.%(ext)s" -f "[ext=mp4][height<=1
 
 alias hist="history | awk -F' ' '{ \$1=\"\"; print }' | fzf --tac --no-sort"
 
-alias snowsql='i3-msg workspace "3" > /dev/null; $XDG_USER_BIN/snowsql'
+alias snowsql='i3-msg workspace "3" > /dev/null; strat -r ubuntu $XDG_USER_BIN/snowsql'
 
 # HOME DIR CLEANUP
 alias nvidia-settings='nvidia-settings --config="$XDG_CONFIG_HOME/nvidia/settings"'
