ce62ac5414f34d14e492eae628c98411f2f68d36
Thales Nunes <thalesaknunes22@gmail.com>
2025-07-23 10:50:14 -0300

    feat: move docstring generation to user commands instead of keybinds

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -54,12 +54,6 @@ V.keymap('n', '<leader>q', ':bdelete<CR>')
 V.keymap('n', '<leader>sr', ':%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>', { silent = false })
 V.keymap('v', '<leader>sr', 'y<esc>:%s/<C-r>"/<C-r>"/g<Left><Left>', { silent = false })
 
--- Reload config
-V.keymap('n', '<leader>r', ':so $MYVIMRC<CR>')
-
--- Add docstrings to current file
-V.keymap('n', '<leader>jg', ':silent !doq --formatter google -w -f %<CR>')
-
 
 if vim.g.vscode then
     V.keymap('n', '<leader>cc', ':VSCodeCommentary<CR>')
--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -5,7 +5,7 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.wrap = false
 
-vim.opt.guicursor = 'n-c:block,v-i-ci-ve:ver25,r-cr:hor20'
+vim.opt.guicursor = "n-c:block,v-i-ci-ve:ver25,r-cr:hor20"
 vim.opt.nu = true
 vim.opt.mouse = "a"
 
@@ -20,11 +20,11 @@ vim.opt.smartcase = true
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
-vim.opt.undodir = vim.fn.stdpath('cache') .. '/undodir'
+vim.opt.undodir = vim.fn.stdpath("cache") .. "/undodir"
 
 vim.opt.scrolloff = 8
 vim.opt.termguicolors = true
-vim.opt.colorcolumn = '80'
+vim.opt.colorcolumn = "80"
 vim.opt.cursorline = true
 
 vim.opt.laststatus = 2
@@ -33,54 +33,55 @@ vim.g.have_nerd_font = true
 
 -- Disable distribution plugins
 local disabled_built_ins = {
-  'netrw',
-  'netrwPlugin',
-  'netrwSettings',
-  'netrwFileHandlers',
-  'gzip',
-  'zip',
-  'zipPlugin',
-  'tar',
-  'tarPlugin',
-  'getscript',
-  'getscriptPlugin',
-  'vimball',
-  'vimballPlugin',
-  '2html_plugin',
-  'logipat',
-  'rrhelper',
-  'spellfile_plugin',
-  'matchit'
+	"netrw",
+	"netrwPlugin",
+	"netrwSettings",
+	"netrwFileHandlers",
+	"gzip",
+	"zip",
+	"zipPlugin",
+	"tar",
+	"tarPlugin",
+	"getscript",
+	"getscriptPlugin",
+	"vimball",
+	"vimballPlugin",
+	"2html_plugin",
+	"logipat",
+	"rrhelper",
+	"spellfile_plugin",
+	"matchit",
 }
 for _, plugin in pairs(disabled_built_ins) do
-  vim.g['loaded_' .. plugin] = 1
+	vim.g["loaded_" .. plugin] = 1
 end
 
-vim.api.nvim_create_user_command('JsonFormatter', "%!jq '.'", {desc='Format current buffer json.'})
+vim.api.nvim_create_user_command( "AddPythonDocstrings", "silent !doq --formatter google -w -f %", { desc = "Add docstrings to all objects in this file." })
+vim.api.nvim_create_user_command("JsonFormatter", "%!jq '.'", { desc = "Format current buffer json." })
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-local yank_group = augroup('highlight_yank', { clear = true })
-local thales_group = augroup('thales', { clear = true })
+local yank_group = augroup("highlight_yank", { clear = true })
+local thales_group = augroup("thales", { clear = true })
 
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            timeout = 40,
-        })
-    end,
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 40,
+		})
+	end,
 })
 
-autocmd('BufWritePre', {
-    group = thales_group,
-    pattern = '*',
-    command = [[%s/\s\+$//e]],
+autocmd("BufWritePre", {
+	group = thales_group,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
 })
 
-vim.cmd [[
+vim.cmd([[
     syntax enable
     filetype plugin on
-]]
+]])
