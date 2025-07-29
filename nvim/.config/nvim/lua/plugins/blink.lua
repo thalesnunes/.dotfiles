6d67546fa4d09142ecffc0992130c3face49ae84
Thales Nunes <thalesaknunes22@gmail.com>
2025-07-29 10:58:02 -0300

    fix: improve blink completion keymaps

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -36,34 +36,14 @@ return {
 		--- @type blink.cmp.Config
 		opts = {
 			keymap = {
-				-- 'default' (recommended) for mappings similar to built-in completions
-				--   <c-y> to accept ([y]es) the completion.
-				--    This will auto-import if your LSP supports it.
-				--    This will expand snippets if the LSP sent a snippet.
-				-- 'super-tab' for tab to accept
-				-- 'enter' for enter to accept
-				-- 'none' for no mappings
-				--
-				-- For an understanding of why the 'default' preset is recommended,
-				-- you will need to read `:help ins-completion`
-				--
-				-- No, but seriously. Please read `:help ins-completion`, it is really good!
-				--
-				-- All presets have the following mappings:
-				-- <tab>/<s-tab>: move to right/left of your snippet expansion
-				-- <c-space>: Open menu or open docs if already open
-				-- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-				-- <c-e>: Hide menu
-				-- <c-k>: Toggle signature help
-				--
-				-- See :h blink-cmp-config-keymap for defining your own keymap
 				preset = "none",
+
 				["<A-space>"] = { "show", "show_documentation", "hide_documentation" },
 				["<C-e>"] = { "hide", "fallback" },
 				["<CR>"] = { "accept", "fallback" },
 
-				["<Tab>"] = { "snippet_forward", "fallback" },
-				["<S-Tab>"] = { "snippet_backward", "fallback" },
+				["<Tab>"] = { "select_next", "fallback" },
+				["<S-Tab>"] = { "select_prev", "fallback" },
 
 				["<A-k>"] = { "select_prev", "fallback" },
 				["<A-j>"] = { "select_next", "fallback" },
@@ -72,10 +52,20 @@ return {
 				["<C-p>"] = { "select_prev", "fallback_to_mappings" },
 				["<C-n>"] = { "select_next", "fallback_to_mappings" },
 
-				["<C-d>"] = { "scroll_documentation_up", "fallback" },
-				["<C-u>"] = { "scroll_documentation_down", "fallback" },
+				["<C-d>"] = { "scroll_documentation_down", "fallback" },
+				["<C-u>"] = { "scroll_documentation_up", "fallback" },
 
 				["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+			},
+
+			cmdline = {
+				keymap = { preset = "inherit" },
+				completion = {
+					menu = { auto_show = true },
+					list = {
+						selection = { preselect = false, auto_insert = true },
+					},
+				},
 			},
 
 			appearance = {
@@ -88,6 +78,9 @@ return {
 				-- By default, you may press `<c-space>` to show the documentation.
 				-- Optionally, set `auto_show = true` to show the documentation after a delay.
 				documentation = { auto_show = false, auto_show_delay_ms = 500 },
+				list = {
+					selection = { preselect = false, auto_insert = true },
+				},
 			},
 
 			sources = {
