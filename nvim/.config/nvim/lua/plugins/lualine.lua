4d7c936117d71fdaf66a62452f971db5ae35e507
Thales Nunes <thalesaknunes22@gmail.com>
2025-07-31 18:30:53 -0300

    feat: lua add macro hinting

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,59 +3,62 @@ return {
 	dependencies = {
 		"kyazdani42/nvim-web-devicons",
 	},
-	config = function()
-		require("lualine").setup({
-			options = {
-				icons_enabled = true,
-				theme = "dracula-nvim",
-				component_separators = { left = "|", right = "|" },
-				section_separators = { left = "", right = "" },
-				disabled_filetypes = {},
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "dracula-nvim",
+			component_separators = { left = "|", right = "|" },
+			section_separators = { left = "", right = "" },
+			disabled_filetypes = {},
+		},
+		sections = {
+			lualine_a = {
+				{
+					"mode",
+					fmt = function(mode)
+						return mode:sub(1, 1)
+					end,
+				},
 			},
-			sections = {
-				lualine_a = {
-					{
-						"mode",
-						fmt = function(mode)
-							return mode:sub(1, 1)
-						end,
-					},
+			lualine_b = { "branch" },
+			lualine_c = {
+				"filename",
+				{
+					"diagnostics",
+					sources = { "nvim_diagnostic" },
 				},
-				lualine_b = { "branch" },
-				lualine_c = {
-					"filename",
-					{
-						"diagnostics",
-						sources = { "nvim_diagnostic" },
-					},
-				},
-				lualine_x = {
-					{
-						"diff",
-						color_added = "#02f702",
-						color_modified = "#efd402",
-						color_removed = "#ea000f",
-					},
-				},
-				lualine_y = {
-					{
-						"lsp_status",
-						icon = "",
-						symbols = {
-							-- Standard unicode symbols to cycle through for LSP progress:
-							spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
-							-- Standard unicode symbol for when LSP is done:
-							done = "✓",
-							-- Delimiter inserted between LSP names:
-							separator = " ",
-						},
-						-- List of LSP names to ignore (e.g., `null-ls`):
-						ignore_lsp = {},
-					},
-					"filetype",
-				},
-				lualine_z = { "location" },
 			},
-		})
-	end,
+			lualine_x = {
+				{
+					require("noice").api.statusline.mode.get,
+					cond = require("noice").api.statusline.mode.has,
+					color = { fg = "#ff9e64" },
+				},
+				{
+					"diff",
+					color_added = "#02f702",
+					color_modified = "#efd402",
+					color_removed = "#ea000f",
+				},
+			},
+			lualine_y = {
+				{
+					"lsp_status",
+					icon = "",
+					symbols = {
+						-- Standard unicode symbols to cycle through for LSP progress:
+						spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+						-- Standard unicode symbol for when LSP is done:
+						done = "✓",
+						-- Delimiter inserted between LSP names:
+						separator = " ",
+					},
+					-- List of LSP names to ignore (e.g., `null-ls`):
+					ignore_lsp = {},
+				},
+				"filetype",
+			},
+			lualine_z = { "location" },
+		},
+	},
 }
