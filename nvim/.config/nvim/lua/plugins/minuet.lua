9beeb6fb38c31e573524979f941ad08de69d9b56
Thales Nunes <thalesaknunes22@gmail.com>
2025-09-20 08:39:51 -0300

    feat: ai completion (minuet) nvim config

---
+++ nvim/.config/nvim/lua/plugins/minuet.lua
@@ -0,0 +1,29 @@
+return {
+	"milanglacier/minuet-ai.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	opts = {
+		provider = "claude",
+		provider_options = {
+			claude = {
+				api_key = "ANTHROPIC_API_KEY",
+				end_point = "https://api.anthropic.com/v1/messages",
+				model = "claude-3-5-haiku-latest",
+				stream = true,
+				max_tokens = 512,
+			},
+		},
+		virtualtext = {
+			auto_trigger_ft = { "*" },
+			keymap = {
+				accept = "<F5>",
+				prev = "<F2>",
+				next = "<F3>",
+				dismiss = "<F4>",
+			},
+			show_on_completion_menu = true,
+		},
+	},
+}
