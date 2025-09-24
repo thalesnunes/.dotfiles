eed6ce5349b54b045c520f50475c4647df933d00
Thales Nunes <thalesaknunes22@gmail.com>
2025-09-23 21:09:29 -0300

    fix: change claude code tmux window name

--- nvim/.config/nvim/lua/plugins/code-bridge.lua
+++ nvim/.config/nvim/lua/plugins/code-bridge.lua
@@ -12,7 +12,7 @@ return {
 	opts = {
 		tmux = {
 			target_mode = "window_name", -- 'window_name', 'current_window', 'find_process'
-			window_name = "cc", -- used when target_mode = 'window_name'
+			window_name = "claude", -- used when target_mode = 'window_name'
 			process_name = "claude", -- used when target_mode = 'current_window' or 'find_process'
 			switch_to_target = true, -- whether to switch to target after sending
 			find_node_process = true, -- whether to look for a node.js process
