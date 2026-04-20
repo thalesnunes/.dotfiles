1f9b02b4fb7b7480ce5b44b90c2a565930088007
Thales Nunes <thalesaknunes22@gmail.com>
2026-04-20 18:47:27 -0300

    feat: add support for / dir config stowing
    small

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -2,37 +2,63 @@
 
 source $DOT/install_utils.sh
 
+is_installed "stow"
+is_installed "awk"
+
 if [ ! -d "$DOT" ]; then
     git clone https://github.com/thalesnunes/.dotfiles "$DOT"
 fi
 
-cd "$DOT"
+function solve_stow_conflicts() {
+    conflicts=$(stow -nt "$target" */ 2>&1 | sed -n 's|.*over existing target \([^ ]*\).*|'"$target"'/\1|p')
 
-is_installed "stow"
-is_installed "awk"
+    if [ -n "$conflicts" ]; then
+        echo "The following conflicts were found:"
+        echo "$conflicts"
 
-conflicts=$(stow -nt ~ */ 2>&1 | sed -n 's|.*over existing target \([^ ]*\).*|'"$HOME"'/\1|p')
-
-if [ -n "$conflicts" ]; then
-    echo "The following conflicts were found:"
-    echo "$conflicts"
-
-    rm_pr="Do you want to REMOVE all the conflicted files? [Y/n]: "
-    mv_pr="Do you want to MOVE all the conflicted files to .old files? [Y/n]: "
-    if yn_pr "$rm_pr"; then
-        echo "$conflicts" | xargs rm -rf
-    elif yn_pr "$mv_pr"; then
-        echo "$conflicts" | xargs mv {} {}.old
+        rm_pr="Do you want to REMOVE all the conflicted files? [Y/n]: "
+        mv_pr="Do you want to MOVE all the conflicted files to .old files? [Y/n]: "
+        if yn_pr "$rm_pr"; then
+            echo "$conflicts" | xargs rm -rf
+        elif yn_pr "$mv_pr"; then
+            echo "$conflicts" | xargs mv {} {}.old
+        else
+            echo "Can't stow with conflicting packages"
+            return 1
+        fi
     else
-        cd "$OLDPWD"
-        exit 0
+        echo "No conflicts found in $target"
     fi
-fi
+    return 0
+}
 
-echo
-stow --no-folding -nvt ~ */
-echo
+function stow_packages() {
 
-yn_pr "Do you accept these changes? [Y/n]: " && stow --no-folding -vt ~ */
+    source="$1"
+    target=$(realpath "$2")
 
-cd "$OLDPWD"
+    cd "$source"
+
+    echo "Solving for $source into $target"
+
+    solve_stow_conflicts "$target"
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+
+    echo
+    stow --no-folding -nvt "$target" */
+    echo
+    if yn_pr "Do you accept these changes? [Y/n]: "; then
+        if [ "$target" == "/" ]; then
+            sudo stow --no-folding -vt "$target" */
+        else
+            stow --no-folding -vt "$target" */
+        fi
+    fi
+
+    cd "$OLDPWD"
+}
+
+stow_packages "$DOT" "$HOME"
+stow_packages "$DOT/root" "/"
