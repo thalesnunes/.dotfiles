5e2935d3479b127083ff3b79787ef80cd6a3948a
Thales Nunes <thalesaknunes22@gmail.com>
2026-05-03 11:40:23 -0300

    feat: standardize keymap setting lazy

--- nvim/.config/nvim/lua/plugins/auto-session.lua
+++ nvim/.config/nvim/lua/plugins/auto-session.lua
@@ -1,9 +1,11 @@
-V.keymap("n", "<leader>fs", ":AutoSession search<CR>")
-
 return {
 	"rmagatti/auto-session",
 	lazy = false,
 	opts = {
 		allowed_dirs = { vim.env.PROJECTS .. "/*", vim.env.WORK_PROJECTS .. "/*" },
 	},
+	config = function(_, opts)
+		require("auto-session").setup(opts)
+		V.keymap("n", "<leader>fs", ":AutoSession search<CR>")
+	end,
 }
