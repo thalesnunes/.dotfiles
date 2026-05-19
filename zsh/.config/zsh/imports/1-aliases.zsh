fd26e5e5be822478f819e3a174f13ba8cf9ca0d2
Thales Nunes <thalesaknunes22@gmail.com>
2026-05-19 17:39:03 -0300

    feat: alias for ssh into homeserver

--- zsh/.config/zsh/imports/1-aliases.zsh
+++ zsh/.config/zsh/imports/1-aliases.zsh
@@ -42,3 +42,5 @@ alias yt_down='yt-dlp -o "~/Downloads/%(title)s.%(ext)s" -f "[ext=mp4][height<=1
 alias hist="history | awk -F' ' '{ \$1=\"\"; print }' | fzf --tac --no-sort"
 
 alias rge="rg --hidden --sort=path -g '!**/.git/**' -g '!**/deployment/**' -g '!**/archive/**' -g '!**/release/**' -g '!**/ddl/**' -i"
+
+alias nta="ssh nta@$(ip neighbour | awk '/f8:da:0c:93:f1:f3/ {print $1}')"
